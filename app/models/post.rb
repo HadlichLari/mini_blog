4fb7e093b33d0b193c6915ff6fa31f60a2b9e470
HadlichLari <hadlich.larissa@gmail.com>
2025-02-02 18:14:51 -0300

    home#index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    validates :title ,presence: true
+    validates :description ,presence: true
+    validates :title ,length: { minimum: 10 }
+    validates :description ,length: { minimum: 30 }
+
+    before_save do 
+        self.title = self.title + "post"
+    end
 end
